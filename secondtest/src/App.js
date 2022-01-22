3046a33cc0966ddff8328b0957e45b6d4947e8db
TLRKiliann <philogenie@protonmail.com>
2022-01-22 02:03:25 +0100

    setup

--- secondtest/src/App.js
+++ secondtest/src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import Navbar from "./components/navbar";
+import Main from "./components/main";
 import './App.css';
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Main />
+      <Navbar />
     </div>
   );
 }
---
+++ secondtest/src/components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return <ul>
+             <li></li>
+             <li></li>
+             <li></li>
+           </ul>
+};
