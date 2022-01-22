794a680776a22198c5fff195657d0ae081a0d0c8
TLRKiliann <philogenie@protonmail.com>
2022-01-22 02:13:42 +0100

    setup

--- secondtest/src/components/navbar.js
+++ secondtest/src/components/navbar.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Navbar() {
     return <ul>
-             <li></li>
-             <li></li>
-             <li></li>
+             <li>first</li>
+             <li>second</li>
+             <li>third</li>
            </ul>
 };
