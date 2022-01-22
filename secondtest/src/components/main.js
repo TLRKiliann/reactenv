eab49730515ccc95e985903f17806f45ff40502c
TLRKiliann <philogenie@protonmail.com>
2022-01-22 02:33:59 +0100

    for some more lines

--- secondtest/src/components/main.js
+++ secondtest/src/components/main.js
@@ -1,5 +1,20 @@
 import React from "react"
 
 export default function Main() {
-	return <h1>My main text</h1>
+    const name = "ko@l@tr33"
+    const lastname = "Test"
+    const date = new Date()
+    const hours = date.getHours() % 12
+    const min = date.getMinutes()
+
+    return (
+          <div>
+            <h2>
+              My main text Name : {name}; Lastname : {lastname}
+            </h2>
+            <h2>
+              Time is {hours}h: {min}min
+            </h2>
+          </div>
+    )
 };
