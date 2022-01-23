abd8cf340db5f5a13e96be57c923459f9e1c67f5
TLRKiliann <philogenie@protonmail.com>
2022-01-23 01:16:37 +0100

    test action js with jsx react

--- secondtest/src/App.js
+++ secondtest/src/App.js
@@ -2,7 +2,12 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import './App.css';
 
+
 function App() {
+  /*const dispName = document.querySelector("card-name");
+  dispName.addEventListener("click", () => {
+    dispName.classList.toggle("card-namejs");
+  });*/
   return (
     <div className="App">
       <header className="App-header">
