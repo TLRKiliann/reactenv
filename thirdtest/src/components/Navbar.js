f533e089cfd0c0a4bdb1d8827a01be6953859ee8
TLRKiliann <philogenie@protonmail.com>
2022-01-23 21:41:11 +0100

    folder setup for react dev mode

---
+++ thirdtest/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>first</li>
+        <li>second</li>
+        <li>third</li>
+      </ul>
+    </nav>
+  )
+};
