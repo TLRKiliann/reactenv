0fe3c90987aeae7ed54e1e2099581e2989ea41ed
TLRKiliann <philogenie@protonmail.com>
2022-01-26 19:29:48 +0100

    component to reach data created

--- fourthtest/src/component/Meme.js
+++ fourthtest/src/component/Meme.js
@@ -1,13 +1,20 @@
 import React from "react"
 
 export default function Meme() {
-  function ...() {
+  //Set empty string by default
+  const [memeImg, setmemeImg] = React.useState("") 
+
+  function getMemeImg() {
+    const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    setmemeImg(memesArray(randomNumber).url)
     e.preventDefault()
   }
+
   return (
     <div>
-      <img src={imgmeme} alt="img-meme" />
-      <button onClick={} className="btn-meme"></button>
+      <img src={memeImg} className="img--conf" alt="img-meme" />
+      <button onClick={getMemeImg} className="btn-meme"></button>
     </div>
   )
 }
