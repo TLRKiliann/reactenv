af4ad5e80486d72d14ab62cf5f3ed274c9059b1b
TLRKiliann <philogenie@protonmail.com>
2022-01-29 22:41:45 +0100

    some modif

--- fifthfolder/src/App.js
+++ fifthfolder/src/App.js
@@ -1,8 +1,17 @@
 import Memes from './component/Memes'
 import Card from './component/Card'
+import numberCard from "./component/numberCard"
 import './App.css';
 
 function App() {
+  const dataCard = numberCard.map(item => {
+    return (
+      <Card
+        id={item.id}
+        name={item.name}
+        age={item.age}
+      />
+  })
   return (
     <div className="App">
       <header className="App-header">
@@ -13,9 +22,12 @@ function App() {
       <main>
         <Memes />
       </main>
-      <Card id={num}/>
+      <div>
+        {dataCard}
+      </div>
     </div>
-  );
-}
+  )
+};
+
 
 export default App;
--- fifthfolder/src/component/Card.js
+++ fifthfolder/src/component/Card.js
@@ -1,12 +1,17 @@
 import React from "react"
-import numberCard from "./numberCard"
 
 export default function Card(props) {
   return (
     <div className="div--myCard">
-      <h3>
+      <h3 key={id}>
         id: {props.id}
       </h3>
+      <p>
+        name: {props.name}
+      </p>
+      <p>
+        age: {props.age}
+      </p>
     </div>
   )
 };
