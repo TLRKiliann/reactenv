0c9014fd021d371c902d24aa327c82d62c37fa7b
TLRKiliann <philogenie@protonmail.com>
2022-02-02 10:54:57 +0100

    show hide answer still on work

--- form_folder/src/component/Card.js
+++ form_folder/src/component/Card.js
@@ -10,26 +10,21 @@ export default function Card() {
 
   function butt(toggleFunc, isShowPunch) {
     return (
-      <button onClick={toggleFunc} className="btn--punch">{!isShowPunch ? "Show" : "Hide"} me answer</button>
+      <button onClick={toggleFunc} className="btn--punch">
+      {isShowPunch ? "Show" : "Hide"} me answer</button>
     )
   }
 
   const questAns = dataCard.map(item => {
     return (
       <div>
-        <p>Question: {item.question}</p>
+        <p key={item.id}>Question: {item.question}</p>
         {isShowPunch && <p>{item.punch}</p>}
         {butt(toggleFunc, isShowPunch)}
       </div>
     )
   })
 
-  /*const answer = dataCard.map(item => {
-    return (
-      <p>Answer: {item.punch}</p>
-    )
-  })*/
-  
   return (
       <div className="punch--div">
         {questAns}
