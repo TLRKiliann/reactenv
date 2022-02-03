7a98bebc032a38e1ced748be24aa32ba49586301
TLRKiliann <philogenie@protonmail.com>
2022-02-03 16:21:15 +0100

    form tag added with funct react

--- form_folder/src/component/Form.js
+++ form_folder/src/component/Form.js
@@ -1,24 +1,127 @@
 import React from "react"
 
 export default function Form() {
-  const [docFormat, setDocFormat] = React.useState(
-    {firstName: "", lastName: "", email: ""}
-    )
+  const [formData, setFormData] = React.useState(
+    {
+      firstName: "",
+      lastName: "",
+      email: "",
+      comments: "",
+      isFriendly: true,
+      employment: "",
+      favColor: ""
+    })
+
+  console.log(formData)
+
+  function handleChange(event) {
+    const {name, value, type, checked} = event.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        //[event.target.name]: event.target.value
+        [name]: type === "checkbox" ? checked : value
+      }
+    })
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        onChange={handleChange}
+        className="first--input"
+        placeholder="Firstname"
+        name="firstName"
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        onChange={handleChange}
+        className="last--input"
+        placeholder="Lastname"
+        name="lastName"
+        value={formData.lastName}
+      />
+      <input
+        type="email"
+        onChange={handleChange}
+        className="last--input"
+        placeholder="Email"
+        name="email"
+        value={formData.email}
+      />
+      <textarea
+        value={formData.comments}
+        onChange={handleChange}
+        placeholder="Comments"
+        name="comments"
+      />
+      <input
+        type="checkbox"
+        id="isFriendly"
+        checked={formData.isFriendly}
+        onChange={handleChange}
+        name="isFriendly"
+      />
+      <label
+        htmlFor="isFriendly">Are you ok?
+      </label>
+      <br />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          chacked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          chacked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          chacked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+      </fieldset>
+      <br />
+      <select
+        id="favColor"
+        value={formData.favColor}
+        onChange={handleChange}
+        name="favColor"
+      >
+        <option value="">--Choose--</option>
+        <option value="red">red</option>
+        <option value="blue">blue</option>
+        <option value="green">green</option>
+      </select>
+
+    </form>
+  )
+}
+  /*
   //const [firstName, setFirstName] = React.useState("")
   //const [lastName, setLastName] = React.useState("")
 
   //console.log(Firstname, Lastname)
-  console.log(docFormat)
-
-
-  function handleChange(event) {
-    setDocFormat(prevDocFormat => {
-      return {
-        ...prevDocFormat,
-        [event.target.name]: event.target.value
-      }
-    })
-  }
 
   /*
   function changeFirstName(event) {
@@ -48,29 +151,3 @@ export default function Form() {
       />
     </form>
   */
-  return (
-    <form>
-      <input
-        type="text"
-        onChange={handleChange}
-        className="first--input"
-        placeholder="Firstname"
-        name="firstName"
-      />
-      <input
-        type="text"
-        onChange={handleChange}
-        className="last--input"
-        placeholder="Lastname"
-        name="lastName"
-      />
-      <input
-        type="email"
-        onChange={handleChange}
-        className="last--input"
-        placeholder="Email"
-        name="email"
-      />
-    </form>
-  )
-}
