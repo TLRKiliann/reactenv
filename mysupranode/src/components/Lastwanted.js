79a835ff4b72dffc9b57cef6b9a6c7ead4e7974b
TLRKiliann <philogenie@protonmail.com>
2022-02-17 00:30:25 +0100

    scipt improved with explanations

--- mysupranode/src/components/Lastwanted.js
+++ mysupranode/src/components/Lastwanted.js
@@ -3,29 +3,45 @@ import dataToFired from './dataToFired'
 
 export default function Lastwanted() {
   const [myLast, setMyLast] = React.useState(dataToFired)
-
+  
+  //To verify access on key into the data file. 
   /*const truck = myLast.map(item => item.fidelity)
   console.log(truck)*/
 
-  const entry = "--> yes ok"
-  const noentry = "--> no good"
+  //Superficiel
+  //const entry = "--> yes ok"
+  //const noentry = "--> no good"
 
   const fidel = myLast.map(item => {
     return (
-      item.fidelity ? `${item.name} ${entry}` : `${item.name} ${noentry}`)
+      <p key={item}>{item.name} : {item.fidelity ? "yes ok" : "no good"}</p>
+    )
   })
-
-  //console.log(fidel)
+  
+  //Or another manner :
+  /*const fidel = myLast.map(item => {
+    return (
+      item.fidelity ? `${item.name} ${entry}` : `${item.name} ${noentry}`)
+  })*/
 
   function callHost() {
-    setMyLast(prevLast => !prevLast.fidelity)
+    setMyLast(prevLast => {
+    return (
+        [...prevLast, !prevLast.fidelity]
+      )
+    })
   }
 
+  console.log('second', setMyLast)
   return (
     <div onClick={callHost} className='subdiv--lastwanted'>
-      {fidel && <p key={myLast.id}>{fidel[0]}</p>}
-      {fidel && <p key={myLast.id}>{fidel[1]}</p>}
-      {fidel && <p key={myLast.id}>{fidel[2]}</p>}
+      {fidel}
     </div>
   )
 }
+
+/*
+{fidel && <p key={myLast.id}>{fidel[0]}</p>}
+{fidel && <p key={myLast.id}>{fidel[1]}</p>}
+{fidel && <p key={myLast.id}>{fidel[2]}</p>}
+*/
