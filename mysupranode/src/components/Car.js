d75b8bdd279be23bf6cef98d0254d7dbbcfcabce
TLRKiliann <philogenie@protonmail.com>
2022-02-19 13:16:10 +0100

    Car is ok with switch carState

--- mysupranode/src/components/Car.js
+++ mysupranode/src/components/Car.js
@@ -1,22 +1,32 @@
 import React from "react";
 
 export default function Car() {
-  const [mod, setModel] = React.useState({brand: 'Ford'})
+  const [switcher, setSwitcher] = React.useState()
+  const [mod, setModel] = React.useState('Ford')
   const [car, setCar] = React.useState({
     id: 1,
     brand: "Ford",
     model: "Mustang",
     year: "1964",
-    color: "red"
+    color: "Blue"
   });
 
+  //add color value at the end of array
+  /*
+  onClick={(e) => updateColor(e)}
 
-  const updateColor = (e) => {
+  function updateColor(e) {
     const val = e.target.value;
     setCar(previousState => {
-      return { 
-        ...previousState, color: val }
+      return {
+        ...previousState, color: val
+      }
     })
+  }
+  */
+
+  function toggle() {
+    setSwitcher(prevSwitch => !prevSwitch)
   }
 
   function changeModel() {
@@ -24,16 +34,28 @@ export default function Car() {
   }
 
   return (
-    <>
-      <h1 onClick={changeModel}>My {mod.brand ? 'Ford' : 'Ferrari'}</h1>
-      <p>
-        It is a {car.color ? 'red' : 'blue'} {car.model} from {car.year}.
-      </p>
+    <div className='div--car'>
+      <h3 onClick={changeModel}>My Model Car is {mod ? 'Ford' : 'Ferrari'}</h3>
+
+      {switcher && <p>
+        It is a {car.model} <mark>{car.color='Red'}</mark> from {car.year}.
+      </p>}
+      {!switcher && <p>
+        It is a {car.model} <mark>{setCar.color ? 'Red' : 'blue'}</mark> from {car.year}.
+      </p>}
+
+      {switcher && <strong>
+        {car.id} {mod ? 'Ford' : 'Ferrari'} {car.year} <mark>{car.color='Red'}</mark>
+      </strong>}
+      {!switcher && <strong>
+        {car.id} {mod ? 'Ford' : 'Ferrari'} {car.year} <mark>{setCar.color ? 'Red' : 'blue'}</mark>
+      </strong>}
+
       <button
         key={car.id}
         type="button"
-        onClick={(e) => updateColor(e)}
-        //onChange={changeModel} doesn't work...
+        //onClick={(e)=> updateColor(e)}
+        onClick={toggle}
         /*onChange={e => {
           const val = e.target.value;
           setCar(prevState => {
@@ -41,11 +63,9 @@ export default function Car() {
           });
         }}*/
       >
-      Blue
+      {switcher ? "Blue" : "Red"}
       </button>
-      <strong>
-        {car.id} {mod.brand ? 'Ford' : 'Ferrari'} {car.year} {car.color ? 'red' : 'blue'}
-      </strong>
-    </>
+
+    </div>
   )
 }
