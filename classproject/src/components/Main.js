9b2129a3859108ad74e60b1a366956038679dee9
TLRKiliann <philogenie@protonmail.com>
2022-02-21 14:27:51 +0100

    onChange with state

--- classproject/src/components/Main.js
+++ classproject/src/components/Main.js
@@ -2,13 +2,17 @@ import React, {useState} from 'react'
 
 export default function Main(props) {
   const [state, setState] = useState()
-  const changeName = () => {
-    setState(prevState => !prevState);
-  }
   return (
-    <div className='div--main'>
-      <p>Love me or love you ? : {state ? 'Me' : 'You'}</p>
-      <button onClick={changeName}> Click me </button>
+    <div>
+      <form>
+        <input
+          type="text"
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+          placeholder="Your Name"
+        />
+        <p>{state}</p>
+      </form>
     </div>
   )
 }
