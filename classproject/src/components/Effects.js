3639ba6c77dba8547c971edb8d7820a2448e54ce
TLRKiliann <philogenie@protonmail.com>
2022-02-21 14:56:40 +0100

    useEffect() practice

--- classproject/src/components/Effects.js
+++ classproject/src/components/Effects.js
@@ -1,5 +1,33 @@
 import React, {useState, useEffect} from 'react'
 
 export default function Effects() {
-	return
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`You have clicked the first button ${count} times`);
+  }, [count]);
+
+  const [count2, setCount2] = useState(0);
+
+  useEffect(() => {
+    console.log(`You have clicked the second button ${count2} times`)
+  }, [count2]);
+
+  return (
+    <div className='div--count'>
+    	<p>Look at the console !!! (F12 or Inspect)</p>
+      <button
+      	onClick={() => setCount(count + 1)}
+      	style={{padding: '10px 10px'}}
+      >
+      	Click me (1)
+      </button>
+      <button
+      	onClick={() => setCount2(count2 + 1)}
+      	style={{padding: '10px 10px'}}
+      >
+      	Click me (2)
+      </button>
+    </div>
+  );
 }
