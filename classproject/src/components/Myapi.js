7f47bbdebf197046f40e9f73cea8f03011b94194
TLRKiliann <philogenie@protonmail.com>
2022-02-22 13:35:18 +0100

    Myapi.js is done

--- classproject/src/components/Myapi.js
+++ classproject/src/components/Myapi.js
@@ -1,27 +1,45 @@
 import React from 'react'
-import {useEffect} from "react";
-import axios from "axios"
+//import axios from "axios"
   
-function Myapi() {
-   
-  useEffect(() => {
-    (async () => {
-      try {
-        const result = await axios.get(
-"https://jsonplaceholder.typicode.com/todos")
-        console.log(result.data);
-        //<h3>return.data.title</h3>
-      } catch (error) {
-        console.error(error);
-      }
-    })()
-  })
-  return (
-    <div >
-        Different ways to fetch Data
-        Title: 
-    </div>
-  );
+class Myapi extends React.Component {
+  // Constructor 
+  constructor(props) {
+      super(props);
+      this.state = {
+          items: [],
+          DataisLoaded: false
+      };
+  }
+  // ComponentDidMount is used to execute the code 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true
+      });
+    })
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded) return <div>
+      <h1> Pleses wait some time.... </h1></div> ;
+ 
+    return (
+      <div className="div--api">
+        <h3> Fetch data from an api in react </h3> {
+          items.map((item) => ( 
+            <nav key={ item.id } >
+              <ol>User_Name: { item.username }</ol>
+              <ol>Full_Name: { item.name }</ol>
+              <ol>User_Email: { item.email }</ol> 
+            </nav>
+          ))
+        }
+      </div>
+    );
+  }
 }
   
 export default Myapi;
