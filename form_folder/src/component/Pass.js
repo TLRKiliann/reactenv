4bf733ef5d67b9478b4c48af493c9603369919a7
TLRKiliann <philogenie@protonmail.com>
2022-02-26 10:28:23 +0100

    just a comment modified

--- form_folder/src/component/Pass.js
+++ form_folder/src/component/Pass.js
@@ -24,7 +24,7 @@ export default function Pass() {
       console.log("Password is ok !")
     } else {
       console.log("No good password...")
-      return //no newsletter if passwd is false...
+      return //if passwd is false no newsletter...
     }
 
     if (passForm.newsLetter === true) {
