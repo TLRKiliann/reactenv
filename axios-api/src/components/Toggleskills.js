0e9b16465222ab994bfbfaa148adea0f1f09ab49
TLRKiliann <philogenie@protonmail.com>
2022-03-05 23:02:48 +0100

    new function for testing toggle

--- axios-api/src/components/Toggleskills.js
+++ axios-api/src/components/Toggleskills.js
@@ -1,72 +1,44 @@
 import React from "react";
 
-export default function Car() {
-  const [switchBut, setSwitchBut] = React.useState()
-  const [mod, setModel] = React.useState('Ford')
+export default function Toggleskills() {
+  const [switchButt, setSwitchButt] = React.useState()
+  const [model, setModel] = React.useState('Mustang')
   const [car, setCar] = React.useState({
     id: 1,
     brand: "Ford",
-    model: "Mustang",
-    year: "1964",
-    color: "Blue"
   });
 
-  //add color value at the end of array
-  
-  onClick={(e) => updateColor(e)}
-
-  function updateColor(e) {
-    const val = e.target.value;
-    setCar(previousState => {
-      return {
-        ...previousState, color: val
-      }
-    })
-  }
-  
-
   function toggle() {
-    setSwitchBut(prevSwitch => !prevSwitch)
+    setSwitchButt(prevSwitch => !prevSwitch)
   }
 
   function changeModel() {
     setModel(prevMod => !prevMod)
   }
+
   return (
     <div className='div--car'>
-      <h3 onClick={changeModel}>My Model Car is {mod ? 'Ford' : 'Ferrari'}</h3>
+      <h3 onClick={changeModel}>My Model Car is {model ? 'Ford' : 'Ferrari'}</h3>
 
-      {switchBut && <p>
-        It is a {car.model} <mark>{car.color ? 'Red' : 'Blue'}</mark> from {car.year}.
-      </p>}
-      {!switchBut && <p>
-        It is a {car.model} <mark>{setCar.color ? 'Red' : 'blue'}</mark> from {car.year}.
-      </p>}
+      {switchButt && <p>It is a {model ? 'Mustang':'405'}</p>}
+      {!switchButt && <p>It is a {model ? 'Mustang':'405'}</p>}
 
-      {switchBut && <strong>
-        {mod ? 'Ford' : 'Ferrari'} {car.year} <mark>
-        {car.color ? 'Red' : 'blue'}</mark>
-      </strong>}
-      {!switchBut && <strong>
-        {mod ? 'Ford' : 'Ferrari'} {car.year} <mark>
-        {setCar.color ? 'Red' : 'blue'}</mark>
-      </strong>}
+      {switchButt && <strong>{model ? 'Ford' : 'Ferrari'}</strong>}
+      {!switchButt && <strong>{model ? 'Ford' : 'Ferrari'}</strong>}
 
       <button
         key={car.id}
         type="button"
-        onClick={(e)=> updateColor(e)}
         onClick={toggle}
-        onChange={e => {
+        /*onChange={e => {
           const val = e.target.value;
           setCar(prevState => {
             return { ...prevState, color: val }
           });
-        }}
+        }}*/
       >
-      {switchBut ? "Blue" : "Red"}
+        {switchButt ? "Blue" : "Red"}
       </button>
-
     </div>
   )
 }
