e864b19935cfbf743ad4a657610b5e0872563725
TLRKiliann <philogenie@protonmail.com>
2022-03-07 18:50:09 +0100

    api-cheerio-express-axios

---
+++ api-expresscheerio/index.js
@@ -0,0 +1,62 @@
+const PORT = 8000
+
+const express = require('express')
+const cheerio = require('cheerio')
+const axios = require('axios')
+
+
+console.log("hello me")
+/*
+const app = express()
+
+const articles = [];
+
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+    base: 'https://www.thetimes.co.uk/'
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.com/environment/climate-crisis',
+    base: 'https://www.theguardian.com/'
+  }
+]
+
+app.get('/news/:newspaperId', async (req, res) => {
+  const newspaperId = req.params.newspaperId
+
+  const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+  console.log('address', newspaperAddress)
+  const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+  console.log('base', newspaperBase)
+
+
+  axios.get(newspaperAddress)
+    .then(response => {
+      const hml = response.data
+      const $ = cheerio.load(html)
+      const specificArticles = []
+
+      $('a:contain("climate")', html).each(function () {
+        const title = $(this).text()
+        const url = $(this).attr('href')
+        specificArticles.push({
+          title,
+          url,
+          url: newspaperBase + url,
+          source: newspaperId
+        })
+    })
+    res.json(specificArticles)
+  }).catch(err => console.log(err))
+})
+
+app.get('/news', (req, res) => {
+  res.json('Welcome to my climate change API')
+})
+
+
+app.listen(PORT, () => console.log(`sever running on port : ${PORT}`))
+*/
