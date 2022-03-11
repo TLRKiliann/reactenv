c51eb37957625cdcb33d65b7d295992a8e1ed1bd
TLRKiliann <philogenie@protonmail.com>
2022-03-11 22:30:20 +0100

    change color with toggle n class

--- other-test/src/components/Thirdcomponent.js
+++ other-test/src/components/Thirdcomponent.js
@@ -5,18 +5,52 @@ export default class Thirdcomponent extends React.Component {
     super(props);
     this.state = {
         thirdobj: props.thirdobj,
-        secItems: props.thirdobjTwo
+        thirdobjTwo: props.thirdobjTwo,
+        dataInput: "",
+        colstyle: "blue",
+        butt: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
-  handleChange() {
-    this.setState()
+  toggle(e) {
+    if (this.state.dataInput.length >= 10) {
+      this.setState(oldState => ({
+        ...oldState,
+        colstyle: !this.state.colstyle
+      }))
+    } else {
+      return alert("Not enough !!!")
+    }
   }
+
+  handleChange(e) {
+    this.setState({dataIput: e.target.value})
+  }
+
   render() {
     return (
       <React.Fragment>
-        <p>{this.state.thirdobj}</p>
-        <p>{this.state.secItems}</p>
+        <h3>Third component file !</h3>
+        <p>{this.props.thirdobj}</p>
+        <p>{this.props.thirdobjTwo}</p>
+        <input
+          value={this.state.dataInput}
+          onChange={(e) => this.setState({dataInput: e.target.value})}
+          placeholder='Enter minimum 10 caracters'>
+        </input>
+        
+        {this.state.colstyle &&  <button 
+          style={{color: 'white', background: this.state.colstyle ? 'red' : 'blue'}} 
+          onClick={e => this.toggle(e)}>
+            10 string
+        </button>}
+        {!this.state.colstyle &&  <button 
+          style={{color: 'white', background: this.setState.colstyle ? 'red' : 'blue'}} 
+          onClick={e => this.toggle(e)}>
+            Save Ok
+        </button>}
+        <p>{this.state.dataInput}</p>
       </React.Fragment>
     );
   }
