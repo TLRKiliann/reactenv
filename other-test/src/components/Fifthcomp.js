acd072f69283abdb3f6573ba50fc9e2fda518dc4
TLRKiliann <philogenie@protonmail.com>
2022-03-12 00:50:33 +0100

    fifth in test

--- other-test/src/components/Fifthcomp.js
+++ other-test/src/components/Fifthcomp.js
@@ -1,24 +1,36 @@
 import React from 'react'
 
-
 export default class Fifthcomp extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			val: []
-		};
-		this.handleChange = this.handleChange.bind(this)
-	}
-	handleChange() {
-		this.setState()
-	}
-	render() {
-		const { val } = this.state;
-		return (
-			<React.Fragment>
-				<h2>Fifth component !</h2>
-				<h3>{val}</h3>
-			</React.Fragment>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      val: true,
+      butt: false,
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e) {
+    this.setState(prevState => !prevState)
+  }
+
+  render() {
+    const { val, butt } = this.state;
+    return (
+      <React.Fragment>
+        <h2>Fifth component !</h2>
+        {butt && <h3>{val ? 'Would you change ?' : 'Keep as you are...'}</h3>}
+        {!butt && <h3>{this.setState.val ? 'Would you change ?' : 'Keep as you are...'}</h3>}
+
+        {butt && <button 
+          onClick={(e) => this.handleChange(e)}>
+          {butt ? "Click to CHANGE !" : "Changing is done !"}
+        </button>}
+        
+        {!butt && <button 
+          onClick={(e) => this.handleChange(e)}>
+          {this.setState.butt ? "Click to CHANGE !" : "Changing is done !"}
+        </button>}
+      </React.Fragment>
+    );
+  }
 }
