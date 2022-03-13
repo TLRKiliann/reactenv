d7f78e16f4acffe7ee6f7d20b7efa2fdb4f4d45b
TLRKiliann <philogenie@protonmail.com>
2022-03-13 14:19:08 +0100

    new component

--- other-test/src/components/Eightcomp.js
+++ other-test/src/components/Eightcomp.js
@@ -1,5 +1,32 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { findDOMNode } from 'react-dom'
+import $ from 'jquery'
 
 export default function Eightcomp() {
+  const [state, setState] = useState('');
 
-}
+  const jQuerycode = () => {
+    const el = findDOMNode(this.ref.toggle) {
+      handleCheck()
+    }
+  }
+
+  useEffect(() => {
+    if (el) {
+    console.log(el);
+    }
+  },[])
+
+  const handleCheck = () => {
+    setState({state: })
+  }
+
+  return (
+    <div>
+      <h4 ref='toggle'>Component with HOOKS !</h4>
+      <button onClick={handleCheck}>Click</button>
+    </div>
+  );
+};
