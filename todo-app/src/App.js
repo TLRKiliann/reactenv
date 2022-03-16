844a3ab610ba57c6926e35d9c27b5bca14e432da
TLRKiliann <philogenie@protonmail.com>
2022-03-16 20:09:06 +0100

    localstorage n state ok

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -7,62 +7,89 @@ export default function App() {
   const [pwd, setPwd] = useState(['']);
   const [chk, setChk] = useState(false);
 
-  function callCheck() {
-  setChk(!chk)
+  function handleCheck() {
+    setChk(!chk)
   }
-  
-  const handle = () => {
-    localStorage.setItem('checkbox', chk);
-    localStorage.setItem('Name', name);
-    localStorage.setItem('Password', pwd);
+
+  const handleRecord = (e) => {
+    if (chk === true) {
+      alert("Ok, wellcome")
+      //{chk && alert("Ok, wellcome")
+      localStorage.setItem('checkbox', chk);
+      localStorage.setItem('Name', name);
+      localStorage.setItem('Password', pwd);
+    } else {
+      alert("You didn't sign terms !")
+    }
+    //}
+    //{!chk && alert("You didn't sign terms")}
+    e.preventDefault();
   };
-  const remove = () => {
+
+  const handleRemove = (e) => {
     localStorage.removeItem('checkbox');
     localStorage.removeItem('Name');
     localStorage.removeItem('Password');
+    alert("Everything has been deleted")
+    e.preventDefault();
   };
     return (
-        <div className="App">
-          <h1>Name of the user:</h1>
+      <div className="App">
+        <form className='sub--app' onSubmit={handleRecord}>
+          <h2>Name of the user :</h2>
           <input
             type='text'
             placeholder="Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
+            className={'input--class'}
           />
-          <h1>Password of the user:</h1>
+          <h2>Password of the user :</h2>
           <input
             type="password"
             placeholder="Password"
             value={pwd}
             onChange={(e) => setPwd(e.target.value)}
+            className={'input--class'}
           />
-          <input
-            type='checkbox'
-            checked={chk}
-            onChange={callCheck}
+          <label className='lbl--input'>
+            <input
+              type='checkbox'
+              checked={chk}
+              onChange={handleCheck}
             />
+              Sign and Submit
+          </label>
           <div>
-            <button onClick={handle}>Done</button>
+            <button type='submit' value='Submit' className='btn--class'>Submit</button>
           </div>
-          {localStorage.getItem('Name') && (
-            <div>
-              Name: <p>{localStorage.getItem('Name')}</p>
+          <div className='local--print'>
+            <div className='sub--local'>
+              {localStorage.getItem('Name') && (
+                <div>
+                  Name : <i>{localStorage.getItem('Name')}</i>
+                </div>
+              )}
             </div>
-          )}
-          {localStorage.getItem('Password') && (
-            <div>
-              Password: <p>{localStorage.getItem('Password')}</p>
+            <div className='sub--local'>
+              {localStorage.getItem('Password') && (
+                <div>
+                  Password : <i>{localStorage.getItem('Password')}</i>
+                </div>
+              )}
             </div>
-          )}
-          {localStorage.getItem('checkbox') && (
-            <div>
-              Checker: <p>{localStorage.getItem('checkbox')}</p>
+            <div className='sub--local'>
+              {localStorage.getItem('checkbox') && (
+                <div>
+                  Checker : <i>{localStorage.getItem('checkbox')}</i>
+                </div>
+              )}
             </div>
-          )}
+          </div>
           <div>
-            <button onClick={remove}>Remove</button>
-         </div>
-      </div>
+            <button onClick={handleRemove} className='btn--class'>Remove</button>
+          </div>
+       </form>
+    </div>
    );
 };
