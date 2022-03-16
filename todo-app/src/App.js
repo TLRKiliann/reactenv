6bc0af6c1c951b89ee79b029a00bd05ddf5a4558
TLRKiliann <philogenie@protonmail.com>
2022-03-16 18:00:34 +0100

    input n checkbox with localstorage ok

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,69 +1,68 @@
-import React, { useState, useEffect } from 'react'
-//import Todolist from './components/Todolist'
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
-  const [todos, setTodos] = useState({
-      isChecked: false,
-      noteRec: ''
-  })
+  const [name, setName] = useState(['']);
+  const [pwd, setPwd] = useState(['']);
+  const [chk, setChk] = useState(false);
 
-  const handleChange = (e) => {
-    const {name, type, value, checked} = e.target
-    setTodos(prevState => ({
-      ...prevState,
-      [name]: type === 'checkbox' ? checked : value
-    }))
-    const { isChecked, noteRec } = todos;
-    localStorage.setItem('isChecked', isChecked)
-    localStorage.setItem('noteRec', noteRec)
+  function callCheck() {
+  setChk(!chk)
   }
-
-  useEffect(() => {
-    const isChecked = localStorage.getItem('isChecked') === 'true';
-    const noteRec = isChecked ? localStorage.getItem('noteRec') : '';
-  },[]);
-
-  const handleSubmit = () => {
-    const { isChecked, noteRec } = todos;
-    localStorage.removeItem('isChecked', isChecked)
-    localStorage.removeItem('noteRec', noteRec);
-    alert("Clean-up ok !")
-  }
-
-  const retrieveData = () => {
-    const isChecked = localStorage.getItem('isChecked') === 'true';
-    const noteRec = isChecked ? localStorage.getItem('noteRec') : '';
+  
+  const handle = () => {
+    localStorage.setItem('checkbox', chk);
+    localStorage.setItem('Name', name);
+    localStorage.setItem('Password', pwd);
   };
-
-  /*const callMap = todos.map(todo => {
-      return <Todolist id={todo.id} />
-  })*/
-
-  return (
-    <form onSubmit={handleSubmit} className="App">
-      <h1>Todo Title</h1>
-
-      <input
-        id="isChecked"
-        type="checkbox"
-        name="isChecked"
-        checked={todos.isChecked}
-        onChange={handleChange}
-      />
-      {todos.isChecked}
-      <input
-        type="text"
-        name="todos"
-        value={todos.noteRec}
-        onChange={(e) => setTodos({noteRec: e.target.value})}
-        className="first--input"
-        placeholder="enter note"
-      />
-      {todos.noteRec}
-      <button type='button' onClick={(e) => handleChange(e)}>Add to do</button>
-      <button type='submit' value='onSubmit'>Clear</button>
-      <button type='button' onClick={retrieveData}>Retrive ALL</button>
-    </form>
-  );
-}
+  const remove = () => {
+    localStorage.removeItem('checkbox');
+    localStorage.removeItem('Name');
+    localStorage.removeItem('Password');
+  };
+    return (
+        <div className="App">
+          <h1>Name of the user:</h1>
+          <input
+            type='text'
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <h1>Password of the user:</h1>
+          <input
+            type="password"
+            placeholder="Password"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+          />
+          <input
+            type='checkbox'
+            checked={chk}
+            onChange={callCheck}
+            />
+          <div>
+            <button onClick={handle}>Done</button>
+          </div>
+          {localStorage.getItem('Name') && (
+            <div>
+              Name: <p>{localStorage.getItem('Name')}</p>
+            </div>
+          )}
+          {localStorage.getItem('Password') && (
+            <div>
+              Password: <p>{localStorage.getItem('Password')}</p>
+            </div>
+          )}
+          {localStorage.getItem('checkbox') && (
+            <div>
+              Checker: <p>{localStorage.getItem('checkbox')}</p>
+            </div>
+          )}
+          <div>
+            <button onClick={remove}>Remove</button>
+         </div>
+      </div>
+   );
+};
