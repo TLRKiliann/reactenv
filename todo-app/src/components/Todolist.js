e0302c68dc6d88ac3eef0a2c9cfe48e2aefe6997
TLRKiliann <philogenie@protonmail.com>
2022-03-17 19:50:03 +0100

    with class and cpDidMount()

--- todo-app/src/components/Todolist.js
+++ todo-app/src/components/Todolist.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
-export default function Todolist(props) {
-  return (
-    <div>
-      <p>{props.id}</p> 
-    </div>
-  )
+export default class LocalStorage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pseudo:'',
+      chk: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+  }
+  handleChange() {
+    this.setState(prevTruc => ({
+      ...prevTruc,
+      chk: !this.state.chk
+    }));
+  }
+
+  handleCheck(e) {
+    const {type,name} = this.state;
+    this.setState(prevState => ({
+      ...prevState,
+      [name]: type === 'checkbox' ? name : value
+    }))
+  }
+  render() {
+    return (
+      <form>
+        <input name='pseudo' value={this.state.pseudo} onChange={(e) => setState({pseudo: e.target.value})}/>
+        <input name='valider' type='checkbox' checked={false} onChange={this.handleChange}/>
+        <buttton type='submit' onClick={this.handleCheck}></button>
+      </form>
+
+    );
+  }
 }
