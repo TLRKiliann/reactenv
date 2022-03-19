df4b62cdde8a0cf138ef0e1e9e98927d94e4371e
TLRKiliann <philogenie@protonmail.com>
2022-03-19 10:56:54 +0100

    reducers setup

--- myfirst-redux/src/App.js
+++ myfirst-redux/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 import './App.css';
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React n Redux</h1>
+        <h3>Counter : {counter}</h3>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        {isLogged ? <h3>Information that we shouldn't see</h3>}
       </header>
     </div>
   );
