158d910a1d205405682ca18706ffe1f893222466
TLRKiliann <philogenie@protonmail.com>
2022-03-23 12:13:39 +0100

    todo setup with uuid.v4

--- todo-app/src/components/TodoForm.js
+++ todo-app/src/components/TodoForm.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useState } from 'react';
-//import { v4 as uuidv4 } from 'uuid';
-//import { v4 as uuidv4 } from 'uuid';
+import  { v4 as uuidv4 } from 'uuid';
 
 
-function TodoForm(addTodo) {
+
+function TodoForm({ addTodo }) {
   const [todo, setTodo] = useState({
     id: '',
     task: '',
@@ -17,8 +17,8 @@ function TodoForm(addTodo) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (todo.task) {
-      addTodo({ ...todo, id: '' }) //uuid.v4()
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuidv4() })
       //reset task input
       setTodo({ ...todo, task: ''})
     }
