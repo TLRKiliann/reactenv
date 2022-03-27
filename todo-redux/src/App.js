2457e073401b0bafa24667392f1becff614076e5
TLRKiliann <philogenie@protonmail.com>
2022-03-27 01:08:06 +0100

    script improved

--- todo-redux/src/App.js
+++ todo-redux/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   const [adder, setAdder] = useState(1);
-  const [substract, setSubstract] = useState(10);
+  const [substract, setSubstract] = useState(2);
   
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
@@ -17,10 +17,8 @@ function App() {
       <input type='text' value={adder} onChange={(e) => setAdder(e.target.value)} />
       <button onClick={() => dispatch(increment(adder))}>Submit</button>
 
-
       <input type='text' value={substract} onChange={(e) => setSubstract(e.target.value)} />
       <button onClick={() => dispatch(decrement(substract))}>Submit</button>
-
 
       {isLogged ? <p>Yes, you are logged !</p> : <p></p>}
       <button onClick={() => dispatch(logger())}>Click</button>
