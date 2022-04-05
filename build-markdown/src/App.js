0917a2fdb0cd4a2184a92a3ce7c6bc3d4d77ab2b
TLRKiliann <philogenie@protonmail.com>
2022-04-05 23:06:39 +0200

    new files

--- build-markdown/src/App.js
+++ build-markdown/src/App.js
@@ -1,78 +1,44 @@
-import React form 'react';
+import React from 'react';
 import { marked } from 'marked';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 // or const { marked } = require('marked');
 import './App.css';
 
 
-function App() {
+//const initialState =
 
-  const html = marked.parse('# Marked in Node.js\n\nRendered by **marked**.');
-  
-  const [edit, setEdit] = useState();
-
-  useEffect(() => {
-    ...
-  }, [])
-
-  function handleChange() {
-    avec html markdown
-    setEdit(e.target.value)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      code: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>
-          Hello !
-        </h2>
-        <h3>{html}</h3>
-        textarea avec un id="editor"
-        <textarea id='editor'>{(e) => setEdit(e.target.value)}</textarea>
-        <p id='preview' value='elem' onChange{handleChange}>{}</p>
-        <p>un élément avec un id="preview ---> is it ok ???</p>
 
-        User Story #3 : Lorsque je saisis du texte dans l'élément #editor, l'élément #preview 
-        est mis à jour au fur et à mesure que je tape pour afficher le contenu de la zone de texte.
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+  render() {
+    const { text } = this.state;
 
-        User Story #4 : Lorsque je saisis du texte markdown à la sauce GitHub dans l'élément 
-        #editor, le texte est rendu sous forme de HTML dans l'élément #preview au fur et à 
-        mesure que je tape
+    const markdown = marked(text);
+
+    return (
+      <div className="App">
+          <h1>Title</h1>
+          <textarea id='editor' value={text} onChange={(e) => this.handleChange(e)} />
+          <div className='col-6 preview' id='preview'>
+            <div>JUSTE POUR VOIR !!!</div>
+
+            <div className='preview' dangerouslySetHTML={{__html: markdown}} />
 
 
-        User Story #5 : Lorsque mon aperçu markdown se charge pour la première fois, le 
-        texte par défaut dans le champ #editor doit contenir du markdown valide qui représente 
-        au moins un de chacun des éléments suivants : un élément de 
-        <h1>title</h1>
-        
-        <h2>sub-title</h2>
-
-        <a href='#'></a>
-
-        bloc de code ???
-
-        <li></li>
-
-        cita en bloc ???
-
-        <img src='' alt='' />
-
-        <p></strong>Bold</strong></p>
-
-        titre (taille H1),  
-        sous-titre (taille H2), 
-        un lien, du code en ligne,
-
-        un bloc de code
-        un element de liste, 
-        une citation en bloc, 
-        une image et 
-        du texte en gras.
-
-        User Story #6 : Lors du premier chargement de ma visionneuse markdown, le markdown par 
-        défaut dans le champ #editor doit être rendu en HTML dans l'élément #preview.
-      </header>
-    </div>
-  );
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
