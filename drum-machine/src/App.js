f517727162389e71942af003cb79c95f16497ffd
TLRKiliann <philogenie@protonmail.com>
2022-04-08 14:55:53 +0200

    setup drum-machine

---
+++ drum-machine/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='sub--app'>
+        <h1>Drum Machine</h1>
+        <div>
+          <h3>Click Buttons</h3>
+
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
