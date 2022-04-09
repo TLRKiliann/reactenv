a2203f1e4e6bfa0f3cb31ddd79954f6ca0cca289
TLRKiliann <philogenie@protonmail.com>
2022-04-09 12:12:26 +0200

    style improved

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,101 +1,110 @@
 import React from 'react';
-//import bootstrap from 'bootstrap';
+import {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
-  play(str) {
-
-    if (str === "q" || str === "Q") {
-      document.getElementById('display').innerText = "Rock on!";
-    } else if (str === "w" || str === "W") {
-      document.getElementById('display').innerText = "Drum Roll!";
-    } else if (str === "e" || str === "E") {
-      document.getElementById('display').innerText = "Music Time!";
-    } else if (str === "a" || str === "A") {
-      document.getElementById('display').innerText = "Big Hit!";
-    } else if (str === "s" || str === "S") {
-      document.getElementById('display').innerText = "Party On!";
-    } else if (str === "d" || str === "D") {
-      document.getElementById('display').innerText = "Jamming on!";
-    } else if (str === "z" || str === "Z") {
-      document.getElementById('display').innerText = "Popstarts!";
-    } else if (str === "x" || str === "X") {
-      document.getElementById('display').innerText = "On the mood!";
-    } else if (str === "c" || str === "C") {
-      document.getElementById('display').innerText = "On the rythm!";
-    }
-    let audio = document.getElementById(str);
-    audio.play();
+const audiClip = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
   }
+];
+
+function App() {
+  return(
+    <div className="main--drum">
+      <h1 className='text-center'>Drum Machine</h1>
+      <div id='drum'>
+        {audiClip.map((clip) => (
+          <PlayPad key={clip.id} clip={clip} />
+          ))}
+      </div>
+    </div>
+  );
+}
+
+
+
+function PlayPad({clip}) {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyPress);
+    return (
+      document.removeEventListener("keydown", handleKeyPress)
+    );
+  });
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === clip.keyCode) {
+      playSound();
+    }
+  };
+
+  const playSound = () => {
+    const audioTag = document.getElementById(clip.keyTrigger);
+    setActive(true);
+    setTimeout(() => setActive(false), 300);
+    audioTag.currentTime = 0;
+    audioTag.play();
+  };
 
   return(
-    <div className="App">
-      <div className='sub--app'>
-        <h1>Drum Machine</h1>
-
-        <div id="drum-machine">
-
-          <div id="display">Click Buttons</div>
-
-
-            <div className='container'>
-              <div className='row'>
-                <button className='drum-pad col' id='unit_sound1' onClick={play('Q')}>
-                  Q
-                  <audio type="audio/mp3" className='clip' id='Q' src='./music/audio1.mp3'/>
-                </button>
-
-                <button className='drum-pad col' id='unit_sound2' onClick={play('W')}>
-                  W
-                  <audio type="audio/mp3" className='clip' id='W' src='./music/audio2.mp3'/>
-                </button>
-
-                <button className='drum-pad col' id='unit_sound3' onClick={play('E')}>
-                  E
-                  <audio type="audio/mp3" className='clip' id='E' src='./music/audio3.mp3'/>
-                </button>
-              </div>
-
-
-              <div className='row'>
-                <button className='drum-pad col' id='unit_sound4' onClick={play('A')}>
-                  A
-                  <audio type="audio/mp3" className='clip' id='A' src='./music/audio4.mp3'/>
-                </button>
-
-                <button className='drum-pad col' id='unit_sound5' onClick={play('S')}>
-                  S
-                  <audio type="audio/mp3" className='clip' id='S' src='./music/audio5.mp3'/>
-                </button>
-
-                <button className='drum-pad col' id='unit_sound6' onClick={play('D')}>
-                  D
-                  <audio type="audio/mp3" className='clip' id='D' src='./music/audio6.mp3'/>
-                </button>
-              </div>
-
-
-              <div className='row'>
-                <button className='drum-pad col' id='unit_sound7' onClick={play('Z')}>
-                  Z
-                  <audio type="audio/mp3" className='clip' id='Z' src='./music/audio7.mp3'/>
-                </button>
-
-                <button className='drum-pad col' id='unit_sound8' onClick={play('X')}>
-                  X
-                  <audio type="audio/mp3" className='clip' id='X' src='./music/audio8.mp3'/>
-                </button>
-
-                <button className='drum-pad col' id='unit_sound9' onClick={play('C')}>
-                  C
-                  <audio type="audio/mp3" className='clip' id='C' src='./music/audio9.mp3'/>
-                </button>
-
-              </div>
-
-            </div>
-          </div>
-
+    <div className="sec--pad">
+      {active && <h3>{clip.id}</h3>}
+      <div onClick={playSound} className='btn btn-secondary p-4 m-4'
+        style={active ? {color: 'orange'} : {color: 'white'}} >
+        <audio className="clip" id={clip.keyTrigger} src={clip.url} />
+        {clip.keyTrigger}
       </div>
     </div>
   );
