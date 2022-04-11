33dc468152e12d3db2e27f7127ddea562e8405b2
TLRKiliann <philogenie@protonmail.com>
2022-04-11 18:13:06 +0200

    basic calculator setup with eval! (don't use eval)

--- js-calculator/src/App.js
+++ js-calculator/src/App.js
@@ -6,28 +6,59 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
-    }
+      counter: '',
+      adder: '+',
+      sub: '-',
+      multi: '*',
+      divide: '/',
+      point: '.'
+    };
     this.add = this.add.bind(this);
     this.substract = this.substract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.division = this.division.bind(this);
+
+    this.handlePoint = this.handlePoint.bind(this);
+
     this.handleClick = this.handleClick.bind(this);
     this.handleClear = this.handleClear.bind(this);
+    this.handleResult = this.handleResult.bind(this);
   }
   
-  handleClick() { // pad nbre
-    this.setState({counter: 1})
+  handleClick(e) {
+    const value = e.target.value;
+    //console.log(value)
+    this.setState(state => ({counter: state.counter += value}))
+    //console.log(this.state.counter)
   }
 
-  add() { //plus 1 func
-    this.setState(state => ({counter: state.counter + 1}));
+  add() { //ajoute un '+' !!!
+    this.setState(state => ({counter: state.counter + state.adder}));
   }
 
   substract() { //moins 1 func
-    this.setState(state => ({counter: state.counter - 1}));
+    this.setState(state => ({counter: state.counter + state.sub}));
   }
-  
+
+  multiply() {
+    this.setState(state => ({counter: state.counter + state.multi}))
+  }
+
+  division() {
+    this.setState(state => ({counter: state.counter + state.divide}))
+  }
+
+  handlePoint() {
+    this.setState(state => ({counter: state.counter + state.point}))
+  }
+
   handleClear() {
-    this.setState({counter: 0})
+    this.setState({counter: ''})
+  }
+
+  handleResult() {
+    this.setState(state => ({counter: eval(state.counter)}))
+    //console.log("length of counter : ", this.state.counter.length)
   }
 
   render() {
@@ -35,34 +66,28 @@ class App extends React.Component {
       <div className="App">
         <h1>Calcultator</h1>
         <div className='calculator'>
-          <button id='zero'>0</button>
+          <button id='zero' value={'0'} onClick={(e) => this.handleClick(e)}>0</button>
 
-          <button id='one' onClick={this.handleClick}>1</button>
-          <button id='two' >2</button>
+          <button id='one' value={'1'} onClick={(e) => this.handleClick(e)}>1</button>
+          <button id='two' value={'2'} onClick={(e) => this.handleClick(e)}>2</button>
 
-          <button id='three' >3</button>
-          <button id='four' >4</button>
-          <button id='five' >5</button>
+          <button id='three' value={'3'} onClick={(e) => this.handleClick(e)}>3</button>
+          <button id='four' value={'4'} onClick={(e) => this.handleClick(e)}>4</button>
+          <button id='five' value={'5'} onClick={(e) => this.handleClick(e)}>5</button>
 
-          <button id='six' >6</button>          
-          <button id='seven' >7</button>
-          <button id='height' >8</button>
+          <button id='six' value={'6'} onClick={(e) => this.handleClick(e)}>6</button>
+          <button id='seven' value={'7'} onClick={(e) => this.handleClick(e)}>7</button>
+          <button id='height' value={'8'} onClick={(e) => this.handleClick(e)}>8</button>
 
-          <button id='nine' >9</button>
-
-
+          <button id='nine' value={'9'} onClick={(e) => this.handleClick(e)}>9</button>
 
           <button id='add' onClick={this.add}>+</button>
           <button id='substract' onClick={this.substract}>-</button>
+          <button id='multiply' onClick={this.multiply}>*</button>
+          <button id='divide' onClick={this.division}>/</button>
 
+          <button id='decimal' onClick={this.handlePoint}>.</button>
 
-
-          <button id='multiply'>*</button>
-          <button id='divide'>/</button>
-
-          <button id='decimal'>.</button>
-        
-            
           <button 
             id='clear'
             onClick={this.handleClear}
@@ -71,7 +96,7 @@ class App extends React.Component {
           </button>
           
 
-          <button id='equals'>=</button>
+          <button id='equals' onClick={this.handleResult}>=</button>
 
 
           <div id="display">
