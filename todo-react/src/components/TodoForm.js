231b43cef41342cb41e2cb87b580d7ce82e4bb62
TLRKiliann <philogenie@protonmail.com>
2022-04-20 00:22:42 +0200

    todo-react setup

---
+++ todo-react/src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+
+function TodoForm({addTodo}) {
+  const [todo, setTodo] = useState({
+    id: '',
+    task: '',
+    completed: false
+  })
+  function handleChange(e) {
+    setTodo({...todo, task: e.target.value})
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.task.trim()) { //trim
+      addTodo({...todo, id: uuidv4()}) //addTodo
+      setTodo({...todo, task: ''})
+    }
+  }
+  // todo.task
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        name='task'
+        value={todo.task}
+        onChange={handleChange} />
+      <button type='submit'>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
---
+++ todo-react/src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Todo from './Todo'
+
+
+function TodoList({todos, toggleComplete, todoRemove}) {
+  return (
+    <ul className={'todo--class'}>
+      {todos.map(todo => (
+        <Todo
+          key={todo.id} //key!!!
+          todo={todo} //def of todo
+          toggleComplete={toggleComplete}
+          todoRemove={todoRemove}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default TodoList;
