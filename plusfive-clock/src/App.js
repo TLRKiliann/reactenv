c4d3d1919966478fe5af094687f49ccad3015be0
TLRKiliann <philogenie@protonmail.com>
2022-04-22 23:23:41 +0200

    so good !

--- plusfive-clock/src/App.js
+++ plusfive-clock/src/App.js
@@ -3,133 +3,142 @@ import { FaRegArrowAltCircleUp, FaRegArrowAltCircleDown,
   FaPlayCircle, FaReact } from "react-icons/fa";
 import { BiReset, BiPause } from "react-icons/bi";
 import "./App.css";
-import $ from "jquery";
+//import $ from "jquery";
 
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      breakCount: 5,
-      sessionCount: 25,
-      sessionMin: 25,
-      sessionSec: 0,
-      timerLabel: 'Session',
-      play: false,
-      intervalID: '',
-      changeCounter: false
+      breakLength: 5,
+      sessionLength: 25,
+      counterClock: 1500,
+      mainTitle: 'Session',
+      isRunning: false,
+      //intervalID: '',
     };
-    this.handleDecrementBreak = this.handleDecrementBreak.bind(this);
-    this.handleIncrementBreak = this.handleIncrementBreak.bind(this);
-    this.handleDecrementSession = this.handleDecrementSession.bind(this);
-    this.handleIncrementSession = this.handleIncrementSession.bind(this);
+    /*this.handleDecrementBreak = this.handleDecrementBreak.bind(this);
+    this.handleIncrementBreak = this.handleIncrementBreak.bind(this);*/
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
     
     this.handleReset = this.handleReset.bind(this);
     this.handlePlayPause = this.handlePlayPause.bind(this);
 
-    this.beginCountDown = this.beginCountDown.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.stopCountDown = this.stopCountDown.bind(this);
-
+    this.handleTimeChange = this.handleTimeChange.bind(this);
     this.changeCounter = this.changeCounter.bind(this);
   }
   
-  handleIncrementSession() {
-    let increment = this.state.sessionCount + 1 > 60 ? 60 : this.state.sessionCount + 1;
-    this.setState({ sessionCount: increment, sessionMin: increment
-    });
-  }
-
-  handleDecrementSession() {
-    let decrement = this.state.sessionCount - 1 < 1 ? 1 : this.state.sessionCount - 1;
-    this.setState({ sessionCount: decrement, sessionMin: decrement
-    });
-  }
-
-  handleIncrementBreak() {
-    this.setState((state) => {
-      return {breakCount: ((state.breakCount + 1 > 60) ? 60 : state.breakCount + 1)}
-    });
-  }
-
-  handleDecrementBreak() {
-    this.setState((state) => {
-      return {breakCount: ((state.breakCount - 1 < 1) ? 1 : state.breakCount - 1)}
-    });
-  }
-
-  handleReset() {
-    if (this.state.intervalID !== "") { // intervalID
-      this.stopCountDown();
-    }
-    let audio = document.getElementById("beep");
-    audio.pause();
-    audio.load();
-
-    this.setState({
-      timerLabel: "Session",
-      breakCount: 5,
-      sessionCount: 25,
-      sessionMin: 25,
-      sessionSec: 0,
-      play: false,
-      intervalID: ""
-    });
-    this.setState({ play: false });
-    /*$(".timer-session-area").addClass("paused");
-    $(".timer-session-area").removeClass("active");
-    $(".timer-session-area").removeClass("break-time");*/
-  }
-
-
-  handlePlayPause() {
-    if (this.state.play) { // Si PAUSE
-      this.setState({ play: false });
-      $(".timer-session-area").addClass("paused");
-      $(".timer-session-area").removeClass("active");
-      $(".timer-session-area").removeClass("break-time");
-      if (this.state.intervalID !== "") {
-        this.stopCountDown();  // Delete intervalID
-      }
-    } else {
-      this.setState({ play: true }); // Si PLAY
-      if (this.state.timerLabel === "Session") {
-        //this.state.play = true;
-        $(".timer-session-area").addClass("active");
-        $(".timer-session-area").removeClass("paused");
-        $(".timer-session-area").removeClass("break-time");
-      } else {
-        this.setState({ play: false });
+  increment = (numb, whichOne) => {
+    const { breakLength, sessionLength, isRunning } = this.state;
+    let count;
+    if (count === 0 && !count && !isRunning) {
+      if (whichOne === 'break') {
+        if (breakLength < 60) {
+          this.setState(state => ({numb: state.numb + 1}));
         }
-      if (this.state.intervalID === "")
-        this.beginCountDown();  // Ajout intervalID
+      }
+      else {
+        if (sessionLength < 60) {
+          this.setState(state => ({numb: state.numb + 1}));
+          if (sessionLength < 9) {
+            this.setState([`0${sessionLength+1}:00`]);
+          }
+          else {
+            this.setState([`${sessionLength+1}:00`]);
+          }    
+        }
+      }   
     }
   }
 
-  handleChange() {
-    let sec = this.state.sessionSec - 1 < 0 ? 59 : this.state.sessionSec - 1;
-    let min = this.state.sessionSec - 1 < 0 ? this.state.sessionMin - 1 : this.state.sessionMin;
-    if(!this.state.changeCounter) {
-      if (min === 0 && sec === 0) {
-      this.setState({ sessionSec: sec, sessionMin: min, changeCounter: true
+  // DECREASE TIMER LENGTH //
+  decrement = (numb, whichOne) => {
+    const { breakLength, sessionLength, isRunning } = this.state;
+    let count;
+    if (count === 0 && !count && !isRunning) {
+      if (whichOne === 'break') {
+        if (breakLength > 1){
+          this.setState(state => ({numb: state.numb -1}));
+        }
+      }
+      else {
+        if (sessionLength > 1) {
+          this.setState(state => ({numb: state.numb -1}));
+          if (sessionLength < 11) {
+            this.setState([`0${sessionLength-1}:00`]);
+          }
+          else {
+            this.setState([`${sessionLength-1}:00`]);
+          }
+        }
+      }
+    }
+  }
+  
+  stopAudio = (id) => {
+    if (this.toSetInterVal) {
+      let newaudio = document.getElementById(id);
+      newaudio.pause();
+      newaudio.currentTime = 0;
+      //console.log(newaudio.currentTime); 
+    }
+  }
+
+  handleReset = () => {
+    clearInterval(this.toSetInterVal);
+    this.stopAudio("beep");
+    this.setState({
+      breakLength: 5,
+      sessionLength: 25,
+      clockCount: 1500,
+      mainTitle: 'Session',
+      isRunning: false,
     });
+  }
+
+  handlePlayPause = () => {
+    const { isRunning } = this.state;
+    if(isRunning) {
+      clearInterval(this.toSetInterVal);
+      this.setState({ isRunning: false });
     } else {
-      this.setState({ sessionSec: sec, sessionMin: min });
-    }
-    } else {
-      this.playAudio();
-      this.changeCounter();
+      this.setState({ isRunning: true });
+      this.toSetInterVal = setInterval(() => {
+        const { 
+          clockCount, 
+          sessionLength, 
+          breakLength, 
+          mainTitle
+        } = this.state;
+        
+        if (timerType === 'session') {
+          this.setState({
+            mainTitle: (mainTitle === 'Session') ? 'Break' : 'Session',
+            clockCount: (mainTitle === 'Session') ? (breakLength * 60) : (sessionLength * 60),
+            audio: this.playAudio('beep')
+          });
+        } else {
+          this.setState({ clockCount: clockCount - 1 });
+        }
+      }, 1000);
     }
   }
 
-  beginCountDown() {
-    let intervalID = setInterval (this.handleChange, 1000);  // handleChange appelé ici
-    this.setState({ intervalID: intervalID }) // PLAY
-  }
-
-  stopCountDown() {
-    clearInterval(this.state.intervalID);
-    this.setState({ intervalID: "" }); // STOP
+  handleTimeChange = (count, timerType) => {
+    const { sessionLength, breakLength, isRunning, mainTitle } = this.state;
+    let newCount;
+    if (timerType === 'session') {
+      newCount = sessionLength + count;
+    } else {
+      newCount = breakLength + count;
+    }
+    if (newCount > 0 && newCount < 61 && !isRunning) {
+      this.setState({ [`${timerType}Count`]: newCount });  
+      if (mainTitle.toLowerCase() === timerType) {
+        this.setState({ clockCount: newCount * 60 })
+      }
+    }
   }
 
   playAudio() {
@@ -138,42 +147,49 @@ export default class App extends React.Component {
   }
 
   changeCounter() {
-    let breakCount = this.state.breakCount;
-    let sessionCount = this.state.sessionCount;
-    if (this.state.timerLabel === "Session") {
-       this.setState({
-        sessionSec: 0, 
-        sessionMin: breakCount, 
-        timerLabel: "Break",
-        changeCounter: false
-    });
-      this.setState({ play: false });
-      /*$(".timer-session-area").addClass("break-time");
-      $(".timer-session-area").removeClass("active");
-      $(".timer-session-area").removeClass("paused");*/
+    let breakLength = this.state.breakLength;
+    let sessionLength = this.state.sessionLength;
+    if (this.state.mainTitle === "Session") {
+      this.setState({
+        counterClock: 1500,
+        mainTitle: "Break",
+        isRunning: false
+      });
+      this.setState({ isRunning: false });
     } else {
-       this.setState({
-        sessionSec: 0, 
-        sessionMin: sessionCount, 
-        timerLabel: "Session", 
-        changeCounter: false
-    });
-      this.setState({ play: true });
-      /*$(".timer-session-area").addClass("active");
-      $(".timer-session-area").removeClass("paused");
-      $(".timer-session-area").removeClass("break-time");*/
+      this.setState({
+        counterClock: 1500,
+        mainTitle: "Session", 
+        isRunning: false
+      });
+      this.setState({ isRunning: true });
     }
   }
 
+  formatMMSS(count) {    
+    let minutes = Math.floor(count / 60);
+    let seconds = count % 60;
+    minutes = minutes < 10 ? ('0' + minutes) : minutes;
+    seconds = seconds < 10 ? ('0' + seconds) : seconds;
+    return `${minutes}:${seconds}`;
+  }
+
   render () {
-    let minutes = this.state.sessionMin + "";
-    let seconds = this.state.sessionSec + "";
-    if (minutes.length < 2) {
-      minutes = "0" + minutes;
+    // render is usefull with multiple calls !
+    const { breakLength, sessionLength } = this.state;
+    /*const { breakLength, sessionLength, clockCount, mainTitle, isRunning } = this.state;
+    const breakOfProps = {
+      title: 'Break',
+      count: breakLength,
+      handleDecrement: () => this.handleTimeChange(-1, 'break'),
+      handleIncrement: () => this.handleTimeChange(1, 'break')
     }
-    if (seconds.length < 2) {
-      seconds = "0" + seconds;
-    }
+    const sessionOfProps = {
+      title: 'Session',
+      count: sessionLength,
+      handleDecrement: () => this.handleTimeChange(-1, 'session'),
+      handleIncrement: () => this.handleTimeChange(1, 'session')
+    }*/
     return (
       <div>
         <div className="react--div">
@@ -191,15 +207,15 @@ export default class App extends React.Component {
               <FaRegArrowAltCircleDown
                 id="break-decrement"
                 size={28}
-                onClick={this.handleDecrementBreak}
+                onClick={() => this.decrement(breakLength, 'break')}
               />
               
-              <span id="break-length">{this.state.breakCount}</span>
+              <span id="break-length">{this.state.breakLength}</span>
 
               <FaRegArrowAltCircleUp
                 id="break-increment"
                 size={28}
-                onClick={this.handleIncrementBreak}
+                onClick={() => this.increment(breakLength, 'break')}
               />
 
             </div>
@@ -215,15 +231,15 @@ export default class App extends React.Component {
               <FaRegArrowAltCircleDown
                 id="Session-decrement"
                 size={28}
-                onClick={this.handleDecrementSession}
+                onClick={() => this.decrement(sessionLength, 'length')}
               />
               
-              <h3 id="Session-length">{this.state.sessionCount}</h3>
+              <h3 id="Session-length">{this.state.sessionLength}</h3>
 
               <FaRegArrowAltCircleUp
                 id="Session-increment"
                 size={28}
-                onClick={this.handleIncrementSession}
+                onClick={() => this.increment(sessionLength, 'length')}
               />
 
             </div>
@@ -235,14 +251,14 @@ export default class App extends React.Component {
             <h1 id="timer-title">25+5 Clock</h1>
 
             <div className="timer-session-area paused">
-              <h2 id="timer-label">{this.state.timerLabel}</h2>
-              <p id="time-left">{minutes}:{seconds}</p>
+              <h2 id="timer-label">{this.state.mainTitle}</h2>
+              <p id="time-left">{this.formatMMSS(this.state.counterClock)}</p>
             </div>
 
             <div className="flex--bottom">
 
               <button id="start_stop" title="play/pause" onClick={this.handlePlayPause}>
-                {this.state.play ? <BiPause id="bi-pause" size={28}/> :
+                {this.state.isRunning ? <BiPause id="bi-pause" size={28}/> :
                   <FaPlayCircle id="fa-play" size={28} />}
               </button>
 
