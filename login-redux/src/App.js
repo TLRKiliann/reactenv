a9a677e3dd098da3fa97f2ebb4dc8bc84cd6a374
TLRKiliann <philogenie@protonmail.com>
2022-04-24 01:34:52 +0200

    new features for a better look!

--- login-redux/src/App.js
+++ login-redux/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {useState, useEffect} from 'react';
 import {actionName, actionPass} from './actions';
 import {useSelector, useDispatch} from 'react-redux';
-//import { FaBeer } from 'react-icons/fa';
+import { AiFillApi, AiFillCodepenCircle } from 'react-icons/ai';
+import { FaGithub, FaFreeCodeCamp, FaReact } from 'react-icons/fa';
+import { SiRedux } from 'react-icons/si';
 import './App.css';
+import Planet from "../src/images/ourplanet.jpg";
 
 function App() {
   const [entryName, setEntryName] = useState('');
@@ -33,42 +36,76 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <p className="first"></p>
-      <p className="second"></p>
-      <p className="third"></p>
-      <p className="fourth"></p>
-      <p className="fifth"></p>
-      <p className="sixth"></p>
-      <div className='reorder--app'>
+    <div className='allinone'>
 
-        <div className='main--divinput'>
-          <h1>Sign In</h1>
-          <h3>{addName}</h3>
-          <h3>{addPass}</h3>
-          <div className='input--div1'>
-            <h4 id="ps-div">Pseudo : </h4>
-            <input
-              type='text'
-              value={entryName}
-              onChange={(e) => setEntryName(e.target.value)}
-              placeholder='pseudo'
-            />
-          </div>
-          <div className='input--div2'>
-            <h4 id="pw-div">Password : </h4>
-            <input
-              type='text'
-              value={entryPass}
-              onChange={(e) => setEntryPass(e.target.value)}
-              placeholder='passwd'
-            />
-          </div>
-        </div>
-          
-        <button onClick={handleClick}>Login</button>
+      <div id="div--planet">
+        <img src={Planet} alt="myplanet"></img>
       </div>
 
+      <div className='main--app'>
+
+        <div className="App">
+
+          <p className="first"></p>
+          <p className="second"></p>
+          <p className="third"></p>
+          <p className="fourth"></p>
+          <p className="fifth"></p>
+          <p className="sixth"></p>
+          <p className="seven"></p>
+
+          <div className='reorder--app'>
+
+            <div className='main--divinput'>
+          
+              <h1>Sign In</h1>
+              <h3>{addName}</h3>
+              <h3>{addPass}</h3>
+              <div className='input--div1'>
+                <h4 id="ps-div">Pseudo : </h4>
+                <input
+                  type='text'
+                  value={entryName}
+                  onChange={(e) => setEntryName(e.target.value)}
+                  placeholder='pseudo'
+                />
+              </div>
+              <div className='input--div2'>
+                <h4 id="pw-div">Password : </h4>
+                <input
+                  type='text'
+                  value={entryPass}
+                  onChange={(e) => setEntryPass(e.target.value)}
+                  placeholder='passwd'
+                />
+              </div>
+            </div>
+            
+            <button onClick={handleClick}>
+              <p>Login</p>
+              <AiFillApi size={28} style={{"color":"white"}} />
+            </button>
+          
+          </div>
+        </div>
+      </div>
+      <div className="icons">
+        <div className="unit--icon">
+          <FaGithub size={42} />
+        </div>
+        <div className="unit--icon">
+          <FaFreeCodeCamp size={42} />
+        </div>
+        <div className="unit--icon">
+          <AiFillCodepenCircle size={42} />
+        </div>
+        <div className="unit--icon">
+          <FaReact size={42} />
+        </div>
+        <div className="unit--icon">
+          <SiRedux size={42} />
+        </div>
+      </div>
     </div>
   );
 }
