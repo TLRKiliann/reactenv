744f2f78836022664d4c1ce070def4130eccb216
TLRKiliann <philogenie@protonmail.com>
2022-04-24 22:54:57 +0200

    some changes

--- login-redux/src/index.js
+++ login-redux/src/index.js
@@ -7,6 +7,7 @@ import allReducers from './reducers';
 import {Provider} from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
+
 const store = createStore(
   allReducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
