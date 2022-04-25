15713e46424a3ff355b2c3de5205dbd5aa110c7b
TLRKiliann <philogenie@protonmail.com>
2022-04-25 14:08:25 +0200

    add lib of react-test, react-jest & Babel

--- plusfive-clock/src/App.test.js
+++ plusfive-clock/src/App.test.js
@@ -1,8 +1,18 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+//import FaRegArrowAltCircleDown from './FaRegArrowAltCircleDown';
 
-test('renders learn react link', () => {
+
+test('test scenario 1', () => {
+   render(<App />);
+   const element = screen.getByText(/App/i);
+   expect(element).toBeInTheDocument();
+});
+
+/*test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
