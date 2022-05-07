839f1fb2bd3868c0a4f77619eb57715071da7c74
TLRKiliann <philogenie@protonmail.com>
2022-05-07 21:21:36 +0200

    test done for counter

--- debug-test/src/App.js
+++ debug-test/src/App.js
@@ -3,15 +3,42 @@ import './App.css';
 
 function App() {
   const [theme, setTheme] = useState("light");
+  const [counter, setCounter] = useState(0);
 
   const toggleTheme = () => {
     const nextTheme = theme === "light" ? "dark" : "light";
     setTheme(nextTheme);
   };
 
-  return <button onClick={toggleTheme}>
-      Current theme: {theme}
-    </button>;
+  const handleCountAdd = () => {
+    setCounter(counter + 1)
+  };
+
+  const handleCountSub = () => {
+    setCounter(counter - 1)
+  };
+
+  return (
+    <div className='main--div'>
+      <div className="sec--div">
+        <h1>Debug & Test</h1>
+      </div>
+      <div className="third--div">
+        <button onClick={handleCountAdd}>
+          +
+        </button>
+        <div id="div--counter">
+          <h3>Current counter {counter}</h3>
+        </div>
+        <button onClick={handleCountSub}>
+          -
+        </button>
+      </div>
+      <button onClick={toggleTheme}>
+        Current theme: {theme}
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- debug-test/src/App.test.js
+++ debug-test/src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
+/*
 test("Test theme button toggle", () => {
   render(<App />);
   const buttonEl = screen.getByText(/Current theme/i);
@@ -9,3 +10,24 @@ test("Test theme button toggle", () => {
   userEvent.click(buttonEl);
   expect(buttonEl).toHaveTextContent(/dark/i);
 });
+*/
+
+/*
+test("Test counter button handleCountAdd", () => {
+  render(<App />);
+  const buttonEl = screen.getByText(/Current counter/i);
+    
+  userEvent.click(buttonEl);
+  expect(buttonEl).toHaveTextContent(/counter/i);
+});
+*/
+
+test("Test counter button handleCountSub", () => {
+  render(<App />);
+  const buttonEl = screen.getByText(/Current counter/i);
+    
+  userEvent.click(buttonEl);
+  expect(buttonEl).toHaveTextContent(/counter/i);
+});
+
+//npm run test
